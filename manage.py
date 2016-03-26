ed1987b0eb71c2283154548eff0463396dd5d077
Parkayun <iamparkayun@gmail.com>
2016-03-26 18:37:02 +0900

    Add db command

--- manage.py
+++ manage.py
@@ -1,15 +1,18 @@
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 from app import create_app
+from app.models import db
 
 
 app = create_app()
+migrate = Migrate(app, db)
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
 def setup_db():
-    from app.models import db
     db.create_all()
 
 

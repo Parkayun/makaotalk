720fd82d21b12c7d5dde31db631d25e278bb068f
Parkayun <iamparkayun@gmail.com>
2016-03-26 18:52:19 +0900

    Add inti, repr method to ChatRoom

--- app/models/chat.py
+++ app/models/chat.py
@@ -5,3 +5,9 @@ class ChatRoom(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(50))
     created_at = db.Column(db.DateTime())
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return '<ChatRoom %d - %r>' % (self.id, self.title)

53c79ca5356df2ac52ffe9b3c0de9c59a2df6030
Parkayun <iamparkayun@gmail.com>
2016-03-26 21:34:31 +0900

    Add Message model

--- app/models/chat.py
+++ app/models/chat.py
@@ -6,8 +6,25 @@ class ChatRoom(db.Model):
     title = db.Column(db.String(50))
     created_at = db.Column(db.DateTime())
 
+    messages = db.relationship('Message', backref='chat_room', lazy='dynamic')
+
     def __init__(self, title):
         self.title = title
 
     def __repr__(self):
         return '<ChatRoom %d - %r>' % (self.id, self.title)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(20))
+    text = db.Column(db.Text())
+    created_at = db.Column(db.DateTime())
+
+    chat_room_id = db.Column(db.Integer(), db.ForeignKey('chat_room.id'))
+
+    def __init__(self, username, text, chat_room_id):
+        self.title, self.text, self.chat_room_id = username, text, chat_room_id
+
+    def __repr__(self):
+        return '<Message %d - %s: %s (ChatRoom: %d)>' % (self.id, self.username, self.text, self.chat_room_id)

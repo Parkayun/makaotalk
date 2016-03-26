2768e65e0b43f9aad09506def2c9b55cc2bbd9d7
Parkayun <iamparkayun@gmail.com>
2016-03-26 18:39:47 +0900

    Set SQLALCHEMY_TRACK_MODIFICATIONS true

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.init_app(app)
 
     from .views import blueprint

ebdb82db3cb76217df50738794837f23a191d30b
Parkayun <iamparkayun@gmail.com>
2016-03-26 20:18:53 +0900

    Add chat room page

--- app/controllers/chat.py
+++ app/controllers/chat.py
@@ -5,6 +5,13 @@ from ..models import db
 from ..models.chat import ChatRoom
 
 
+@blueprint.route('/chat/<int:room_id>/')
+def chat(room_id):
+    chat_room = ChatRoom.query.get_or_404(room_id)
+    data = {'chat_room': chat_room}
+    return render_template('chat/chat.html', **data)
+
+
 @blueprint.route('/chat/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':

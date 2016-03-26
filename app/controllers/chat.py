64a023a24a521468b3b0243665a1b780308eedaf
Parkayun <iamparkayun@gmail.com>
2016-03-26 19:05:52 +0900

    Add creating chat room feature and page

--- app/controllers/chat.py
+++ app/controllers/chat.py
@@ -1,7 +1,20 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from . import blueprint
+from ..models import db
 from ..models.chat import ChatRoom
+
+
+@blueprint.route('/chat/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title', '')
+        if title != '':
+            chat_room = ChatRoom(title)
+            db.session.add(chat_room)
+            db.session.commit()
+            return redirect(url_for('controllers.index'))
+    return render_template('chat/create.html')
 
 
 @blueprint.route('/')

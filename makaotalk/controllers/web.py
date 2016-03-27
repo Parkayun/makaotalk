e55b47bbefce32d302de96ed6ab3b1b311fb6e0d
Parkayun <iamparkayun@gmail.com>
2016-03-27 12:12:13 +0900

    Keep PEP8

--- makaotalk/controllers/web.py
+++ makaotalk/controllers/web.py
@@ -6,15 +6,6 @@ from ..utils.decorators import get_or_set_username
 from ..models.chat import ChatRoom, Message
 
 
-@blueprint.route('/chat/<int:room_id>/')
-@get_or_set_username
-def web_chat_room(room_id):
-    chat_room = ChatRoom.query.get_or_404(room_id)
-    messages = Message.query.filter_by(chat_room=chat_room)
-    data = {'chat_room': chat_room, 'messages': messages, 'username': session['username']}
-    return render_template('chat/chat.html', **data)
-
-
 @blueprint.route('/chat/create/', methods=('GET', 'POST'))
 def web_chat_create():
     if request.method == 'POST':
@@ -25,6 +16,15 @@ def web_chat_create():
             db.session.commit()
             return redirect(url_for('controllers.web_chat_room', room_id=chat_room.id))
     return render_template('chat/create.html')
+
+
+@blueprint.route('/chat/<int:room_id>/')
+@get_or_set_username
+def web_chat_room(room_id):
+    chat_room = ChatRoom.query.get_or_404(room_id)
+    messages = Message.query.filter_by(chat_room=chat_room)
+    data = {'chat_room': chat_room, 'messages': messages, 'username': session['username']}
+    return render_template('chat/chat.html', **data)
 
 
 @blueprint.route('/')
